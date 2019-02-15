8324021a9ccd7ea821147b32e8def1c579a6f221
Sherief Farid <sherief@farid.cloud>
2019-02-15 21:21:35 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,40 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+
+def won?(board)
+
+  # return false if board is empty
+
+  # Return false if board is full and no win (draw) - see #full?
+
+  # Compaire to WIN_COMBINATIONS and return winning combinations if any are met for both X and Y
+
+end
+
+def full?(board)
+    # check the board status
+    #Return false if the board is not full
+    # Return true if the board is not full
+    # Tip use .each
+end
+
+def draw?(board)
+  # Return true if the board is full and not won?
+  # return false if the board is not won nad the board is not full
+  # return return false if the board in won
+
+
+end
+
+def over?(board)
+  # return true if the the board is won or draw or is full
+  if won?(board == true || draw?(board) == true || full(board) == true)
+    return true
+  end
+end
+
+def winner?(board)
+  # return X or O based on who won the game
+end
